462a0514f94a1addd6d7a3c22b83284d174e6547
andrescarvajal3 <andrescarvajal3@gmail.com>
2022-02-23 10:51:43 -0500

    segundo commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = Hola;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = cambiodos;
 
 // Crea una variable booleana:
 const nuevoBool = null;
